eee4b0828f74bd20ca20a1b1c62ed1c63491f996
Alex <ruski550@gmail.com>
2018-02-06 05:43:23 -0600

    Updated with all the proper buttons, not just the test ones. The formatting got a little messed up, but still works up through Android 7, Android 8 still does not work

--- FiSwitcher/app/src/main/java/com/example/fiswitcher/MainActivity.java
+++ FiSwitcher/app/src/main/java/com/example/fiswitcher/MainActivity.java
@@ -10,7 +10,10 @@ package com.example.fiswitcher;
         import android.os.Bundle;
         import android.view.View;
         import android.widget.Button;
+        import android.content.BroadcastReceiver;
+        import android.content.BroadcastReceiver;
 
+//TODO messed up the formatting in Activity main a little bit, take a look at that later
 public class MainActivity extends AppCompatActivity {
 
     //TODO Find out how to auto ask the user for permission that it does not to be toggled in the settings menu
@@ -20,16 +23,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //adds the autobutton
+        //adds the autobutton (Actually the test button)
         Button autoBtn = (Button) findViewById(R.id.autoBtn);
         //add the secret code button for testing
         Button secretCodeTestBtn = (Button) findViewById(R.id.secretCodeTestBtn);
+        //Adds the sprint button
+        Button sprintBtn = (Button) findViewById(R.id.SprintBtn);
+        //Adds the US Cellular Button
+        Button uscBtn = (Button) findViewById(R.id.USCBtn);
+        //Add the T-Mobile Button
+        Button tmobileBtn = (Button) findViewById(R.id.TMobileBtn);
+        //Adds the autochose button
+        Button autochoseBtn = (Button) findViewById(R.id.AutoChoseBtn);
+
+
+
+
         //When the autoBtn is pressed
         autoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Should now call the preprogrammed number
-                //Need to look at this a bi t more
+                //Need to look at this a bit more
 
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
                 //Set as my phone number for now for testing purposes
@@ -84,6 +99,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Calls specified number when sprint button is clicked
+        sprintBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendBroadcast(new Intent("android.provider"));
+
+                sendBroadcast(new Intent("android.provider.Telephony.SECRET_CODE", Uri.parse("android_secret_code://34777")));
+
+            }
+        });
+
+        //Calls specified number when USC button is clicked
+        uscBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendBroadcast(new Intent("android.provider"));
+
+                sendBroadcast(new Intent("android.provider.Telephony.SECRET_CODE", Uri.parse("android_secret_code://34872")));
+
+            }
+        });
+        //Calls specified number when tmobile button is clicked
+        tmobileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendBroadcast(new Intent("android.provider"));
+
+                sendBroadcast(new Intent("android.provider.Telephony.SECRET_CODE", Uri.parse("android_secret_code://34866")));
+
+            }
+        });
+
+        //Calls specified number when autchose button is clicked
+        autochoseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendBroadcast(new Intent("android.provider"));
+
+                sendBroadcast(new Intent("android.provider.Telephony.SECRET_CODE", Uri.parse("android_secret_code://342886")));
+
+            }
+        });
 
 
 
